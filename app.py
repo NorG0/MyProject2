985b1643106ae75aff5d04c33d70cd5dcb95f48d
Nam Hoi <91311560+NorG0@users.noreply.github.com>
2022-05-20 11:19:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ import plotly.express as px
 
 
 
-st.write("asd")
+st.title("HOẠT ĐỘNG LÀM VIỆC")
+menu = ["Home","Login","SignUp"]
+choice = st.sidebar.selectbox("Menu",menu)
+task = st.selectbox("Task",["Dashboard","Profiles","Manage"])
 
